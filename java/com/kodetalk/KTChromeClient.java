b369e10cb59f826e8b221464966c981ff9e901a2
Kodetalk <kodetalk@gmail.com>
2017-08-16 22:15:12 +0530

    Code for memory leak optimized.

---
+++ java/com/kodetalk/KTChromeClient.java
@@ -0,0 +1,31 @@
+package com.kodetalk;
+
+import android.graphics.Color;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+/**
+ * Created by Deb on 8/16/2017.
+ */
+
+public class KTChromeClient extends WebChromeClient {
+
+    private ProgressBar progressBar;
+
+    public KTChromeClient(ProgressBar progressBar) {
+        this.progressBar = progressBar;
+    }
+
+    public void onProgressChanged(WebView webView, int progress) {
+        progressBar.setProgress(progress);
+        progressBar.getProgressDrawable()
+                .setColorFilter(Color.parseColor("#283653"), android.graphics.PorterDuff.Mode.MULTIPLY);
+        if (progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+            progressBar.setVisibility(ProgressBar.VISIBLE);
+        }
+        if (progress == 100) {
+            progressBar.setVisibility(ProgressBar.GONE);
+        }
+    }
+}
